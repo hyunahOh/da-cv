c7a10fb5296f6c3931ef6f4f7519c348e92cfca2
hyunahOh <dowkkr94@gmail.com>
2020-04-30 18:26:26 +0900

    Updates

---
+++ precache-manifest.696143c179123a6addbdce8b849ced5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dac507d43db3c1aaa72ee7c5fc41277",
+    "url": "/da-cv/index.html"
+  },
+  {
+    "revision": "ce998afbdcffab4699e2",
+    "url": "/da-cv/static/css/2.6b00acf2.chunk.css"
+  },
+  {
+    "revision": "d00c0c5ed4ef2437ade0",
+    "url": "/da-cv/static/css/main.6ac63d70.chunk.css"
+  },
+  {
+    "revision": "ce998afbdcffab4699e2",
+    "url": "/da-cv/static/js/2.e0521425.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/da-cv/static/js/2.e0521425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00c0c5ed4ef2437ade0",
+    "url": "/da-cv/static/js/main.b8698dd9.chunk.js"
+  },
+  {
+    "revision": "b69d6887fc11a3acb6f6",
+    "url": "/da-cv/static/js/runtime-main.dd1f1b37.js"
+  }
+]);
